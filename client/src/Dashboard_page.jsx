6a23dc785e2eecdc6c28f8dee16654334d7c0b06
ab-ruh <abell.demere@gmail.com>
2024-03-28 22:14:47 +0300

    dash board implementation

---
+++ client/src/Dashboard_page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Navbar from "./components/Navbar/Navbar";
+
+const Dashboard_page = () => {
+  return (
+    <div>
+      <Navbar />
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Dashboard_page;
