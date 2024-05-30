e85bd974b9b6e7e8146703143b20c9d33ac26c14
Iyasu2 <iyasuaworku@gmail.com>
2024-05-30 21:13:19 +0300

    'deploy'

--- server/sequelize.js
+++ server/sequelize.js
@@ -6,13 +6,18 @@ const pg = require("pg");
 // Set up connection pooling
 pg.defaults.poolSize = 10;
 
-const sequelize = new Sequelize("rent", "postgres", "Magna1new", {
-  host: "localhost",
-  dialect: "postgres",
-  define: {
-    timestamps: false, // Disable timestamps for simplicity
-  },
-  dialectModule: pg,
-});
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DATABASE,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.POSTGRES_USER,
+    define: {
+      timestamps: false, // Disable timestamps for simplicity
+    },
+    dialectModule: pg,
+  }
+);
 
 module.exports = sequelize;
